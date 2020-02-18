beb8e537c03d577526d8acfe7228b01b68601569
Gaurav Parthasarathy <gauravp@ethz.ch>
2020-02-18 17:56:15 +0100

    added more term convenience methods

--- Source/ProofGeneration/Isa/IsaCommonTerms.cs
+++ Source/ProofGeneration/Isa/IsaCommonTerms.cs
@@ -4,19 +4,35 @@ namespace ProofGeneration.Isa
 {
     class IsaCommonTerms
     {
+
+        private static readonly TermIdent SomeId = TermIdentFromName("Some");
+        private static readonly TermIdent NoneId = TermIdentFromName("None");
+        private static readonly TermIdent FstId = TermIdentFromName("fst");
+        private static readonly TermIdent SndId = TermIdentFromName("snd");
+
         public static Term SomeOption(Term arg)
         {
-            return new TermApp(TermIdentFromName("Some"), new List<Term>() { arg });
+            return new TermApp(SomeId, new List<Term>() { arg });
         }
 
         public static Term NoneOption()
         {
-            return new TermApp(TermIdentFromName("None"), new List<Term>());
+            return new TermApp(NoneId, new List<Term>());
+        }
+
+        public static Term Fst(Term arg)
+        {
+            return new TermApp(FstId, new List<Term>() { arg });
+        }
+
+        public static Term Snd(Term arg)
+        {
+            return new TermApp(SndId, new List<Term>() { arg });
         }
 
         public static TermIdent TermIdentFromName(string name)
         {
             return new TermIdent(new SimpleIdentifier(name));
-        }
+        }       
     }
 }
