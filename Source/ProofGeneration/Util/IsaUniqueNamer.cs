1741e1e9a88044646b16f818bcb00eaa5c0a6d1b
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2020-03-02 20:54:11 +0100

    wrapper around unique namer for Isabelle identifiers

---
+++ Source/ProofGeneration/Util/IsaUniqueNamer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ProofGeneration.Util
+{
+    public class IsaUniqueNamer
+    {
+        private readonly Microsoft.Boogie.VCExprAST.UniqueNamer uniqueNamer;
+
+        private readonly string spacer;
+
+        public IsaUniqueNamer(string spacer)
+        {
+            this.spacer = spacer;
+            uniqueNamer = new Microsoft.Boogie.VCExprAST.UniqueNamer()
+            {
+                Spacer = spacer
+            };
+        }
+
+        public IsaUniqueNamer() : this("_") { }
+
+        public string GetName(object obj, string preferredName)
+        {
+            return uniqueNamer.GetName(obj, GetValidIsaString(preferredName));
+        }
+
+        public string GetLocalName(object obj, string preferredName)
+        {
+            return uniqueNamer.GetLocalName(obj, GetValidIsaString(preferredName));
+        }
+
+        public void PushScope()
+        {
+            uniqueNamer.PushScope();
+        }
+
+        public void PopScope()
+        {
+            uniqueNamer.PopScope();
+        }
+        
+        private string GetValidIsaString(string s)
+        {
+            Regex firstChar = new Regex("^[A-Za-z]");
+
+            string sMod = s;
+
+            if (!firstChar.IsMatch(s))
+                sMod = "isa" + spacer + s;
+
+            Regex illegalCharacters = new Regex("[@#^*!&]");            
+
+            return illegalCharacters.Replace(sMod, spacer);
+        }
+
+    }
+}
