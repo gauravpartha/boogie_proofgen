f7e4134cc0a206281df977e6691e05230336aa2e
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2020-03-05 16:03:58 +0100

    added zip do method in BasicUtil
    taken from stackoverflow post

--- Source/ProofGeneration/Util/BasicUtil.cs
+++ Source/ProofGeneration/Util/BasicUtil.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace ProofGeneration.Util
 {
-    class BasicUtil
+    public static class BasicUtil
     {
 
         public static void Add<T, R>(T key, R val, IDictionary<T, IList<R>> dict)
@@ -20,6 +20,19 @@ namespace ProofGeneration.Util
         public static void AddEquation<T>(T lhs, T rhs, IList<Tuple<IList<T>, T>> equations)
         {
             equations.Add(new Tuple<IList<T>, T>(new List<T>() { lhs }, rhs));
+        }
+
+        //Taken from https://stackoverflow.com/a/18396163
+        public static void ZipDo<T1, T2>(this IEnumerable<T1> first, IEnumerable<T2> second, Action<T1, T2> action)
+        {
+            using (var e1 = first.GetEnumerator())
+            using (var e2 = second.GetEnumerator())
+            {
+                while (e1.MoveNext() && e2.MoveNext())
+                {
+                    action(e1.Current, e2.Current);
+                }
+            }
         }
 
     }
