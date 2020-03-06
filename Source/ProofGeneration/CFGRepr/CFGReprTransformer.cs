d9092a5829b735e984918a55fd831fe67d33bc5b
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2020-03-06 10:16:35 +0100

    adjust CFG rep transformer to copy blocks if desired
    the copies are done on a semi-shallow basis, need to see what kind of copies are required exactly
    it is ensured that the blocks in the transfer commands are not copied again (i.e., only a single copy per block)

--- Source/ProofGeneration/CFGRepr/CFGReprTransformer.cs
+++ Source/ProofGeneration/CFGRepr/CFGReprTransformer.cs
@@ -3,26 +3,58 @@ using Microsoft.Boogie.GraphUtil;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
+using ProofGeneration.Util;
+using System.Reflection;
+using System;
 
 namespace ProofGeneration.CFGRepresentation
 {
-    class CFGReprTransformer
+    public static class CFGReprTransformer
     {
 
-        static public CFGRepr getCFGRepresentation(Implementation impl)
+        private static readonly MethodInfo CloneMethod = typeof(Object).GetMethod("MemberwiseClone", BindingFlags.NonPublic | BindingFlags.Instance);
+
+
+        //blocks are not copied
+        public static CFGRepr getCFGRepresentation(Implementation impl)
         {
-            AlternateCFGRepr(impl, out Block entryBlock, out IDictionary < Block, IList < Block >> outgoingBlocks);
-            IDictionary<Block, int> labeling = GetTopologicalLabeling(impl);
+            return getCFGRepresentation(impl, false, out IDictionary<Block, Block> newToOldMapping);
+        }
+
+        //if "generateCopy", then blocks will be copied and the mapping from the copied blocks to the original blocks is given by "newToOldBlocks"
+        public static CFGRepr getCFGRepresentation(Implementation impl, bool generateCopy, out IDictionary<Block, Block> newToOldBlocks)
+        {
+            Contract.Requires(impl != null);
+            Contract.Ensures((generateCopy && newToOldBlocks != null) || (!generateCopy && newToOldBlocks == null));
+
+            IList<Block> blocksToConvert;
+
+            if(generateCopy)
+            {
+                blocksToConvert = CopyBlocks(impl.Blocks);
+
+                var newToOldInternal = new Dictionary<Block, Block>();
+                blocksToConvert.ZipDo(impl.Blocks, (bNew, bOld) => newToOldInternal.Add(bNew, bOld));
+                newToOldBlocks = newToOldInternal;
+            } else
+            {
+                blocksToConvert = impl.Blocks;
+                newToOldBlocks = null;
+            }                
+
+            AlternateCFGRepr(blocksToConvert, out Block entryBlock, out IDictionary < Block, IList < Block >> outgoingBlocks);
+            IDictionary<Block, int> labeling = GetTopologicalLabeling(blocksToConvert);
+ 
             return new CFGRepr(outgoingBlocks, labeling, entryBlock);
         }
 
-        static private void AlternateCFGRepr(Implementation impl, out Block entryBlock, out IDictionary<Block, IList<Block>> outgoingBlocks)
+        private static void AlternateCFGRepr(IList<Block> blocks, out Block entryBlock, out IDictionary<Block, IList<Block>> outgoingBlocks)
         {
             entryBlock = null;
             int blockNum = 0;
             outgoingBlocks = new Dictionary<Block, IList<Block>>();
 
-            foreach (var block in impl.Blocks)
+            foreach (var block in blocks)
             {
                 if (block.Predecessors.Count == 0)
                 {
@@ -50,17 +82,17 @@ namespace ProofGeneration.CFGRepresentation
             }
         }
 
-        static private IDictionary<Block, int> GetTopologicalLabeling(Implementation impl)
+        private static IDictionary<Block, int> GetTopologicalLabeling(IList<Block> blocks)
         {
-            Contract.Requires(impl != null);
-            Contract.Ensures(impl.Blocks.Count == Contract.Result<IDictionary<Block, int>>().Count);
+            Contract.Requires(blocks != null);
+            Contract.Ensures(blocks.Count == Contract.Result<IDictionary<Block, int>>().Count);
             Contract.Ensures(Contract.Result<IDictionary<Block, int>>().Values.Min() == 0 && 
-                             Contract.Result<IDictionary<Block, int>>().Values.Max() == impl.Blocks.Count-1);
+                             Contract.Result<IDictionary<Block, int>>().Values.Max() == blocks.Count-1);
 
             //adusted code from VC.cs
             Graph<Block> dag = new Graph<Block>();
-            dag.AddSource(impl.Blocks[0]);
-            foreach (Block b in impl.Blocks)
+            dag.AddSource(blocks[0]);
+            foreach (Block b in blocks)
             {
                 GotoCmd gtc = b.TransferCmd as GotoCmd;
                 if (gtc != null)
@@ -86,5 +118,44 @@ namespace ProofGeneration.CFGRepresentation
 
             return retLabels;
         }
+
+        private static IList<Block> CopyBlocks(IList<Block> blocks)
+        {
+            //shallow copy of each block + update edges to copied blocks
+            //TODO:  need to make sure this is sufficient
+            IDictionary<Block, Block> oldToNewBlock = new Dictionary<Block, Block>();
+
+            IList<Block> copyBlocks = new List<Block>();
+
+            foreach (Block b in blocks)
+            {
+                Block copyBlock = (Block)CloneMethod.Invoke(b, null);
+                copyBlocks.Add(copyBlock);
+                oldToNewBlock.Add(b, copyBlock);
+            }
+
+            //make sure block references are updated accordingly
+            foreach (Block copyBlock in copyBlocks)
+            {
+                if (copyBlock.TransferCmd is GotoCmd gtc)
+                {
+                    var newSuccessors = gtc.labelTargets.Select(succ => oldToNewBlock[succ]).ToList();
+                    GotoCmd gotoCmdCopy = (GotoCmd)CloneMethod.Invoke(gtc, null);
+                    gotoCmdCopy.labelTargets = newSuccessors;
+                    copyBlock.TransferCmd = gotoCmdCopy;
+                } else
+                {
+                    copyBlock.TransferCmd = (TransferCmd)CloneMethod.Invoke(copyBlock.TransferCmd, null);                    
+                }
+
+                if (copyBlock.Predecessors != null)
+                {
+                    copyBlock.Predecessors = copyBlock.Predecessors.Select(succ => oldToNewBlock[succ]).ToList();                    
+                }
+            }
+
+            return copyBlocks;
+        }      
+
     }
 }
