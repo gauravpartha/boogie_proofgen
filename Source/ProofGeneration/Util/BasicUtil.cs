71b986648b753577688efd493b2e7f391390aa17
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2020-03-16 18:43:58 +0100

    AddRange extension method for IList

--- Source/ProofGeneration/Util/BasicUtil.cs
+++ Source/ProofGeneration/Util/BasicUtil.cs
@@ -35,5 +35,24 @@ namespace ProofGeneration.Util
             }
         }
 
+        //Taken from https://stackoverflow.com/a/33104162
+        public static void AddRange<T>(this IList<T> list, IEnumerable<T> items)
+        {
+            if (list == null) throw new ArgumentNullException(nameof(list));
+            if (items == null) throw new ArgumentNullException(nameof(items));
+
+            if (list is List<T> asList)
+            {
+                asList.AddRange(items);
+            }
+            else
+            {
+                foreach (var item in items)
+                {
+                    list.Add(item);
+                }
+            }
+        }
+
     }
 }
