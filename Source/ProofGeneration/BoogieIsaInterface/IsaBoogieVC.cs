772e57dc0ddad6b85a02064cd6a60f7deb3afb3e
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2020-10-03 22:42:45 +0200

    rename IsaBoogieVCTerm -> IsaBoogieVC

---
+++ Source/ProofGeneration/BoogieIsaInterface/IsaBoogieVC.cs
@@ -0,0 +1,58 @@
+﻿using ProofGeneration.Isa;
+using System;
+
+namespace ProofGeneration.BoogieIsaInterface
+{
+    public static class IsaBoogieVC
+    {
+        public static TermIdent VCInvId { get; } = IsaCommonTerms.TermIdentFromName("vc_inv_closed");
+        public static TermIdent VCTypeId { get; } = IsaCommonTerms.TermIdentFromName("vc_type_of_val");
+        public static TermIdent TyToClosedId { get; } = IsaCommonTerms.TermIdentFromName("ty_to_closed");
+        public static TermIdent ClosedToTyId { get; } = IsaCommonTerms.TermIdentFromName("closed_to_ty");
+
+        private static TermIdent TPrimClosedId { get;  } = IsaCommonTerms.TermIdentFromName("TPrimC");
+        
+        public static Term VCInv(int idx)
+        {
+            return new TermApp(VCInvId, new NatConst(idx));
+        }
+
+        public static Term VCType(Term absValTyMap)
+        {
+            return new TermApp(VCTypeId, absValTyMap);
+        }
+
+        public static Term VCTypeConstructor(string constrName, int nArgs)
+        {
+            //TODO: do not limit number of arguments
+            if(nArgs > 5)
+            {
+                throw new NotImplementedException("Do not support more than 5 type constructor arguments currently.");
+            }
+
+            var id = IsaCommonTerms.TermIdentFromName("vc_type_constr" + nArgs);
+            return new TermApp(id, new StringConst(constrName));
+        }
+        
+        public static TypeIsa BoogieClosedType()
+        {
+            return IsaCommonTypes.GetDataTypeNoArg("closed_ty");
+        }
+        
+        public static Term PrimTypeClosed(Term primType)
+        {
+            return new TermApp(TPrimClosedId, primType);
+        }
+
+        public static Term TyToClosed(Term ty)
+        {
+            return new TermApp(TyToClosedId, ty);
+        }
+
+        public static Term ClosedToTy(Term ty)
+        {
+            return new TermApp(ClosedToTyId, ty);
+        }
+
+    }
+}
