977430534e9b53bc5d95b5d50c2a60d3bc63bea6
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2021-04-22 10:06:30 +0200

    adapt ProofGenSubsetChecker

--- Source/ProofGeneration/Util/ProofGenSubsetChecker.cs
+++ Source/ProofGeneration/Util/ProofGenSubsetChecker.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Linq;
+using System.Transactions;
 using Isabelle.Ast;
 using Microsoft.Boogie;
 using Type = Microsoft.Boogie.Type;
 
 namespace ProofGeneration.Util
 {
+    /// <summary>
+    /// Checks in a whether the input program is potentially supported by proof generation.
+    /// </summary>
     public class ProofGenSubsetChecker : ResultReadOnlyVisitor<Term>
     {
         private object problematicNode;
@@ -12,10 +17,14 @@ namespace ProofGeneration.Util
 
         protected override bool TranslatePrecondition(Absy node)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
-        public bool ProofGenSupportsSubset(Program p, out object resultNode)
+        /// <summary>
+        /// If false is returned, then the input is not supported.
+        /// If true is returned, then the input is potentially supported.
+        /// </summary>
+        public bool ProofGenPotentiallySupportsSubset(Program p, out object resultNode)
         {
             problematicNode = null;
             Visit(p);
@@ -23,30 +32,89 @@ namespace ProofGeneration.Util
             return problematicNode == null;
         }
 
-        public override Choice VisitChoice(Choice node)
+        public override Procedure VisitProcedure(Procedure node)
+        {
+            //no procedure type parameters, free pre- and postconditions, no where clauses
+            if (node.TypeParameters.Any() || 
+                node.Requires.Any(req => req.Free) || 
+                node.Ensures.Any(ens => ens.Free) ||
+                node.InParams.Union(node.OutParams).Any(v => v.TypedIdent.WhereExpr != null))
+            {
+                problematicNode = node;
+                return node;
+            }
+
+            return base.VisitProcedure(node);
+        }
+
+        public override Implementation VisitImplementation(Implementation node)
+        {
+            //no where clauses
+            if (node.LocVars.Any(v => v.TypedIdent.WhereExpr != null))
+            {
+                problematicNode = node;
+                return node;
+            }
+
+            return base.VisitImplementation(node);
+        }
+
+        public override Type VisitBasicType(BasicType node)
+        {
+            //only support integers and booleans as basic types
+            if (node.IsReal || node.isFloat || node.IsBv || node.IsString || node.IsRMode || node.IsRegEx)
+            {
+                problematicNode = node;
+                return node;
+            }
+
+            return base.VisitBasicType(node);
+        }
+        
+        public override Type VisitFloatType(FloatType node)
         {
             problematicNode = node;
             return node;
         }
 
+        #region regex
+        //do not support regular expressions
         public override Sequential VisitSequential(Sequential node)
         {
             problematicNode = node;
             return node;
         }
 
-        public override Type VisitBvType(BvType node)
+        public override Choice VisitChoice(Choice node)
         {
             problematicNode = node;
             return node;
         }
+        
+        public override Cmd VisitRE(RE node)
+        {
+            problematicNode = node;
+            return node;
+        }
+        
+        public override AtomicRE VisitAtomicRE(AtomicRE node)
+        {
+            problematicNode = node;
+            return node;
+        }
+        #endregion
 
+        
+        //do not support code block expressions
         public override Expr VisitCodeExpr(CodeExpr node)
         {
             problematicNode = node;
             return node;
         }
 
+        #region maps 
+        
+        //do not support maps
         public override Expr VisitLambdaExpr(LambdaExpr node)
         {
             problematicNode = node;
@@ -59,24 +127,28 @@ namespace ProofGeneration.Util
             return node;
         }
 
-        public override Cmd VisitRE(RE node)
+        public override AssignLhs VisitMapAssignLhs(MapAssignLhs node)
         {
             problematicNode = node;
             return node;
         }
 
-        public override Cmd VisitStateCmd(StateCmd node)
+        public override Type VisitMapTypeProxy(MapTypeProxy node)
         {
             problematicNode = node;
             return node;
         }
-
-        public override YieldCmd VisitYieldCmd(YieldCmd node)
+        #endregion
+        
+        #region bitvectors
+        
+        //do not support bitvectors
+        public override Type VisitBvType(BvType node)
         {
             problematicNode = node;
             return node;
         }
-
+        
         public override Expr VisitBvConcatExpr(BvConcatExpr node)
         {
             problematicNode = node;
@@ -94,35 +166,22 @@ namespace ProofGeneration.Util
             problematicNode = node;
             return node;
         }
-
-        public override AssignLhs VisitMapAssignLhs(MapAssignLhs node)
+        #endregion
+        
+        #region civl
+        
+        //do not support concurrent intermediate verification language (CIVL) features
+        public override YieldCmd VisitYieldCmd(YieldCmd node)
         {
             problematicNode = node;
             return node;
         }
-
-        public override Type VisitMapTypeProxy(MapTypeProxy node)
-        {
-            problematicNode = node;
-            return node;
-        }
-
+        
         public override Cmd VisitParCallCmd(ParCallCmd node)
         {
             problematicNode = node;
             return node;
         }
-
-        public override Type VisitFloatType(FloatType node)
-        {
-            problematicNode = node;
-            return node;
-        }
-
-        public override AtomicRE VisitAtomicRE(AtomicRE node)
-        {
-            problematicNode = node;
-            return node;
-        }
+        #endregion
     }
 }
