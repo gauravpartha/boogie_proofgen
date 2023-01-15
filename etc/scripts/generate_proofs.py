a1b2f8bd9185debff96f04d0e030c26cf1049791
gauravpartha <49554253+gauravpartha@users.noreply.github.com>
2023-01-15 15:59:06 +0100

    Add basic CI (#7)
    added CI to generate and check proofs for benchmarks

--- etc/scripts/generate_proofs.py
+++ etc/scripts/generate_proofs.py
@@ -1,14 +1,11 @@
 import os
 import subprocess
-from datetime import datetime
 import argparse
 
-# The following command should point to the Boogie proof generation binary 
-boogie_proofgen_bin = "boogieproof"
-
-def generate_proofs(input_dir, output_dir):
+def generate_proofs(input_dir, output_dir, boogie_proofgen_bin):
     n_success = 0
     n_failure = 0
+    n_ignored = 0
 
     # turn input directory path into an absolute path, since we are going to 
     # change the working directory
@@ -21,19 +18,28 @@ def generate_proofs(input_dir, output_dir):
         for file in files:
             if file.endswith('.bpl'):
                 boogie_file_path = os.path.join(root, file)
-            
-                # check whether Boogie can produce certificates
-                errorcode = subprocess.run([boogie_proofgen_bin, boogie_file_path],stdout=subprocess.DEVNULL)
-                if(errorcode.returncode == 0):
-                    print("Generated proofs for: " + boogie_file_path)
-                    n_success += 1
-                else:
-                    print("Could not generate proofs for: " + boogie_file_path)
-                    n_failure += 1
-                    exit()
+
+                with open(boogie_file_path) as f:
+                    first_line = f.readline().strip('\n')
+                    if(first_line != "//:: ProofGen(IgnoreFile)"):
+                        # check whether Boogie can produce certificates
+                        errorcode = subprocess.run([boogie_proofgen_bin, boogie_file_path],stdout=subprocess.DEVNULL)
+                        if(errorcode.returncode == 0):
+                            print("Generated proofs for: " + boogie_file_path)
+                            n_success += 1
+                        else:
+                            print("Could not generate proofs for: " + boogie_file_path)
+                            n_failure += 1
+                            exit(1)
+                    else:
+                        #ignore file
+                        print("Ignored :" + boogie_file_path)
+                        n_ignored += 1
+                        pass
                     
     print("Generated proofs for " + str(n_success) + " tests")
     print("Could not generate proofs for " + str(n_failure) + " tests")
+    print("Ignored " + str(n_ignored) + " tests")
 
 def main():
     parser = argparse.ArgumentParser()
@@ -49,6 +55,12 @@ def main():
         required=True
     )
 
+    parser.add_argument(
+        "-b", "--boogieproofExe",
+        help="Path to Boogie proof generation executable (must be an absolute path or a command that can be executed from any directory)",
+        default="boogieproof"
+    )
+
     args = parser.parse_args()
 
     if (not(os.path.isdir(args.inputdir))):
@@ -58,8 +70,8 @@ def main():
     if (os.path.exists(args.outputdir)):
         print("The desired path " +  args.outputdir + " for the output directory already exists")
         exit(1)
-
-    generate_proofs(args.inputdir, args.outputdir)
+    
+    generate_proofs(args.inputdir, args.outputdir, args.boogieproofExe)
 
 if __name__ == '__main__':
     main()
