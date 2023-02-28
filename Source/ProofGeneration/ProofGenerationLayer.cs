a7e8865cdf7bd5937103e5e1c0066f2c9952b004
gauravpartha <gaurav.parthasarathy@inf.ethz.ch>
2023-02-28 09:21:11 +0100

    minor fix

--- Source/ProofGeneration/ProofGenerationLayer.cs
+++ Source/ProofGeneration/ProofGenerationLayer.cs
@@ -446,6 +446,8 @@ namespace ProofGeneration
         /// </summary>
         public static void VcIsTrivial()
         {
+            if (CommandLineOptions.Clo.OnlyGenerateInitialProgramIsa())
+                return;
           vcHintManager.TransformHintsToTrivialVc();
         }
 
